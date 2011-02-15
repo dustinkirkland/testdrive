08ab53246ba4aba1b13fad8c6b6544d6279ae96e
Andres Rodriguez <andreserl@ubuntu.com>
2011-02-15 17:54:39 -0500

    * bin/testdrive: qemu image output of file changed. Update accordingly. * testdrive-gtk: Add Messaging Indicator.   - indicator/testdrive-gtk: Add   - debian/testdrive-gtk.install: Install indicator.   - setup.py: Update installer to do the same as above.

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
 	name='testdrive',
-	version='3.9',
+	version='3.10',
 	license='GPL-3',
 	author='Andres Rodriguez',
 	author_email='andreserl@ubuntu.com',
@@ -110,7 +110,8 @@ DistUtilsExtra.auto.setup(
 			('share/testdrivegtk/ui', glob('data/ui/*.xml')),
 			('share/testdrivegtk/media', glob('data/media/*.png')),
 			('share/testdrivegtk/media', glob('data/media/*.svg')),
-			('share/pixmaps', glob('data/media/testdrive-gtk.xpm'))
+			('share/pixmaps', glob('data/media/testdrive-gtk.xpm')),
+			('share/indicators/messages/applications', glob('indicator/testdrive-gtk'))
 			],
 	cmdclass={'install': InstallAndUpdateDataDirectory}
 	)
