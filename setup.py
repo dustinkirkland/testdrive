baf3dcc2b20559f83b3b5669ce5f26d48e1f5e09
Dustin Kirkland <kirkland@ubuntu.com>
2015-08-06 15:21:48 -0500

    opening 3.28

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='testdrive',
-    version='3.27',
+    version='3.28',
     license='GPL-3',
     author='Andres Rodriguez',
     author_email='andreserl@ubuntu.com',
